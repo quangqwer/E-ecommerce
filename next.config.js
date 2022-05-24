b20d204a62f5f33a1e5779b28c9f8a9009c2b729
ctv.thonghv <quangqwer12357@hmail.com>
2022-05-24 09:36:36 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://devpq:quang2000@cluster0.dngi4.mongodb.net/Ecommerce_nextjs?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET": "dW'Q>({BMP]rF5:J/=JGU9=G=f:=g$a$]mh}wEq85va_Yd^n;H",
+    "REFRESH_TOKEN_SECRET": "[$]t\y@r4s?u`e&e6:wbc{#S:@C@u{L9yR>Q6X!YzDvR\sL933~H<@XPR\RRT\cS%'W%^^:c~SN#bxP`",
+    "PAYPAL_CLIENT_ID": "AaiMS0gHD9BifUCotRb6l56I6ET0kcAzjE0B0g1CtNL-qWTt9OINTV5xUq6MABx_AVVOTXZi6DlDBJG5",
+    "CLOUD_UPDATE_PRESET": "nextjs_store",
+    "CLOUD_NAME": "pmq-clound",
+    "CLOUD_API": "https://api.cloudinary.com/v1_1/pmq-clound/image/upload"
+  }
+}
---
+++ pages/profile.js
@@ -0,0 +1,198 @@
+import Head from 'next/head'
+import { useState, useContext, useEffect } from 'react'
+import Link from 'next/link'
+import { DataContext } from '../store/GlobalState'
+
+import valid from '../utils/valid'
+import { patchData } from '../utils/fetchData'
+
+import { imageUpload } from '../utils/imageUpload'
+
+const Profile = () => {
+    const initialState = {
+        avatar: '',
+        name: '',
+        password: '',
+        cf_password: '',
+    }
+    const [data, setData] = useState(initialState)
+    const { avatar, name, password, cf_password } = data
+
+    const { state, dispatch } = useContext(DataContext)
+    const { auth, notify, orders } = state
+
+    useEffect(() => {
+        if (auth.user) setData({ ...data, name: auth.user.name })
+    }, [auth.user])
+
+    const handlerChange = (e) => {
+        const { name, value } = e.target
+        setData({ ...data, [name]: value })
+        dispatch({ type: 'NOTYFY', payload: {} })
+    }
+
+    const handlerUpdateProfile = e => {
+        e.preventDefault()
+        if (password) {
+            const errMsg = valid(name, auth.user.email, password, cf_password)
+
+            if (errMsg) return dispatch({ type: 'NOTIFY', payload: { error: errMsg } })
+            updatePassword()
+        }
+        if (name !== auth.user.name || avatar) updateInfo()
+    }
+
+    const updatePassword = (name, email) => {
+        dispatch({ type: 'NOTIFY', payload: { loading: true } })
+        patchData('user/resetPassword', { password }, auth.token)
+            .then(res => {
+                if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+                return dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+            })
+    }
+
+    const changeAvatar = (e) => {
+        const file = e.target.files[0]
+        if (!file) return dispatch({ type: 'NOTIFY', payload: { error: 'File does not exist.' } })
+
+        if (file.size > 1024 * 1024 * 5) return dispatch({ type: 'NOTIFY', payload: { error: 'The largest img size is 5mb.' } })
+
+        if (file.type !== "image/jpeg" && file.type !== "image/png")
+            return dispatch({ type: 'NOTIFY', payload: { error: 'Image format is incorrect' } })
+
+        setData({ ...data, avatar: file })
+    }
+
+    const updateInfo = async () => {
+        let media
+        // dispatch({ type: 'NOTIFY', payload: { loading: true } })
+
+        if (avatar) media = await imageUpload([avatar])
+
+        patchData('user', {
+            name, avatar: avatar ? media[0].url : auth.user.avatar
+        }, auth.token).then(res => {
+            if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
+
+            dispatch({
+                type: 'AUTH', payload: {
+                    token: auth.token,
+                    user: res.user
+                }
+            })
+            return dispatch({ type: 'NOTIFY', payload: { success: res.msg } })
+        })
+
+    }
+
+    if (!auth.user) return null;
+    return (
+        <div className="profile_page">
+            <Head>
+                <title>Profile</title>
+            </Head>
+
+            <section className="row text-secondary my-3">
+                <div className="col-md-4">
+                    <h3 className="text-center text-uppercase">
+                        {auth.user.role === 'user' ? 'User Profile' : 'Admin Profile'}
+                    </h3>
+                    <div className="avatar">
+                        <img src={avatar ? URL.createObjectURL(avatar) : auth.user.avatar}
+                            alt="avatar" />
+                        <span>
+                            <i className="fa fa-camera"></i>
+                            <p>Change</p>
+                            <input type="file" name="file" id="file_up"
+                                accept='image/*'
+                                onChange={changeAvatar}
+                            />
+                        </span>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" value={name} className="form-control"
+                            placeholder="Your name" onChange={handlerChange} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" defaultValue={auth.user.email}
+                            className="form-control" placeholder="Your name" disabled={true} onChange={handlerChange} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">New Password</label>
+                        <input type="password" name="password" value={password} className="form-control"
+                            placeholder="Your new password" onChange={handlerChange} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="cf_password">Confirm new Password</label>
+                        <input type="password" name="cf_password" value={cf_password} className="form-control"
+                            placeholder="Confirm new password" onChange={handlerChange} />
+                    </div>
+
+                    <button className="btn btn-info" disabled={notify.loading}
+                        onClick={handlerUpdateProfile}>
+                        Update
+                    </button>
+                </div>
+                <div className="col-md-8 table-responsive">
+                    <h3 className="text-uppercase">Orders</h3>
+                    <div className="my-3 table-responsive">
+                        <table className="table-bordered table-hover w-100 text-uppercase"
+                            style={{ minWidth: '600px', cursor: 'pointer' }}>
+                            <thead className="bg-light font-weight-bold">
+                                <tr>
+                                    <td className="p-2">id</td>
+                                    <td className="p-2">date</td>
+                                    <td className="p-2">total</td>
+                                    <td className="p-2">delivered</td>
+                                    <td className="p-2">paid</td>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {
+                                    orders.map(order => (
+                                        <tr key={order._id}>
+                                            <td className="p-2">
+                                                <Link href={`/order/${order._id}`}>
+                                                    <a> {order._id}</a>
+                                                </Link>
+                                            </td>
+                                            <td className="p-2">
+                                                {new Date(order.createdAt).toLocaleDateString()}
+                                            </td>
+                                            <td className="p-2">${order.total}</td>
+                                            <td className="p-2">
+                                                {
+                                                    order.delivered
+                                                        ? <i className="fas fa-check text-success"></i>
+                                                        : <i className="fas fa-times text-danger"></i>
+                                                }
+                                            </td>
+                                            <td className="p-2">
+                                                {
+                                                    order.paid
+                                                        ? <i className="fas fa-check text-success"></i>
+                                                        : <i className="fas fa-times text-danger"></i>
+                                                }
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+
+export default Profile
